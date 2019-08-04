4f85248e28e0f5c42d824bb27b51afa446fa21b4
wata_mac <watassbass@gmail.com>
2019-08-05 00:33:49 +0900

    :space_invader:

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const serverOptions: ServerOptions = {
+		command: "tflint",
+		args: ["--langserver"],
+	}
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tflint-lsc" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: ['terraform'],
+	};
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	const disposable = new LanguageClient("TFLint", serverOptions, clientOptions).start()
 
 	context.subscriptions.push(disposable);
 }
